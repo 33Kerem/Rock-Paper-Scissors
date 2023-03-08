db45c96eab16c1ade6fd846619bebbe627f3f0cf
Kerem Üzer <keremuzer33@gmail.com>
2023-03-08 19:57:25 +0300

    add playRound function

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 function getComputerChoice(){
-    const choices = ["Rock", "Paper", "Scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
+  const choices = ["Rock", "Paper", "Scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection){
+  if(computerSelection.toUpperCase() == playerSelection.toUpperCase()){
+    console.log("Tie!")
+  }
+  else if(computerSelection === "Rock" && playerSelection.toUpperCase() === "SCISSORS"){
+    console.log("You Lose! Rock beats Scissors");
+  }
+  else if(computerSelection === "Rock" && playerSelection.toUpperCase() === "PAPER"){
+    console.log("You Win! Paper beats Rock");
+  }
+  else if(computerSelection === "Paper" && playerSelection.toUpperCase() === "SCISSORS"){
+    console.log("You Win! Scissors beats Paper");
+  }
+  else if(computerSelection === "Paper" && playerSelection.toUpperCase() === "ROCK"){
+    console.log("You Lose! Paper beats Rock");
+  }
+  else if(computerSelection === "Scissors" && playerSelection.toUpperCase() === "PAPER"){
+    console.log("You Lose! Scissors beats Paper");
+  }
+  else if(computerSelection === "Scissors" && playerSelection.toUpperCase() === "ROCK"){
+    console.log("You Win! Rock beats Scissors");
+  }
 }
