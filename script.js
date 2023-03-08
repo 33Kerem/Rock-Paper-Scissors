0e58fc07631d00026225e03551b5fba351e55d86
Kerem Üzer <keremuzer33@gmail.com>
2023-03-08 20:11:14 +0300

    add game() function

--- script.js
+++ script.js
@@ -10,20 +10,43 @@ function playRound(playerSelection, computerSelection){
   }
   else if(computerSelection === "Rock" && playerSelection === "SCISSORS"){
     console.log("You Lose! Rock beats Scissors");
+    return "computer";
   }
   else if(computerSelection === "Rock" && playerSelection === "PAPER"){
     console.log("You Win! Paper beats Rock");
+    return "player";
   }
   else if(computerSelection === "Paper" && playerSelection === "SCISSORS"){
     console.log("You Win! Scissors beats Paper");
+    return "player";
   }
   else if(computerSelection === "Paper" && playerSelection === "ROCK"){
     console.log("You Lose! Paper beats Rock");
+    return "computer";
   }
   else if(computerSelection === "Scissors" && playerSelection === "PAPER"){
     console.log("You Lose! Scissors beats Paper");
+    return "computer";
   }
   else if(computerSelection === "Scissors" && playerSelection === "ROCK"){
     console.log("You Win! Rock beats Scissors");
+    return "player";
   }
+}
+
+function game(){
+  let computer, player = 0;
+  for (let i = 0; index < 5; i++) {
+    if(playRound() === "computer"){
+      computer++;
+    }
+    else if (playRound === "player") {
+      player ++;
+    }
+    else{
+      computer++;
+      player++;
+    }
+  }
+  console.log(`you: ${player} computer: ${computer}`);
 }
